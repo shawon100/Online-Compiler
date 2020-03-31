dc39742bee70103bf92b096f19b28cb7d311b042
pranaylobo <42401887+pranaylobo@users.noreply.github.com>
2020-04-01 03:45:52 +0530

    Update c.php

--- compilers/c.php
+++ compilers/c.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: *');  
+header('Access-Control-Allow-Origin: http://localhost:4200/editor');  
         
     
         $postdata = file_get_contents("php://input");
